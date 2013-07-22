3f19623cf7f7782984ec9541a869921c73cadc71
memaschow <memaschow@gmail.com>
2013-07-22 12:17:31 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mauserl::Application.config.secret_key_base = '931ace36b55f98e4ff0dd15e541ad4a1d1348cabd799e74d22014f86ec7d3f4c4f9dc242e1bd4f4f2d649800dd30cefcbbaea9d33d8f643acea342076b0e5d6c'
